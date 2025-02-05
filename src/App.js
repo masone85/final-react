ca2da2d7e68836fd67bddc9294956d32a8083ea6
Emily <113108800+masone85@users.noreply.github.com>
2025-02-05 18:56:15 +0100

    added github repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Emily Mason and is open-sourced on GitHub{" "}
+        <a href="https://github.com/masone85/final-react" target="_blank">
+          here!
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
