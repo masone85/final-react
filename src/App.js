917930f012ab1b5e5ed8f3d1f0088f2648add283
Emily <113108800+masone85@users.noreply.github.com>
2025-02-07 11:13:28 +0100

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Emily Mason and is open-sourced on GitHub{" "}
-        <a href="https://github.com/masone85/final-react" target="_blank">
-          here!
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Emily Mason and is open-sourced on GitHub{" "}
+          <a href="https://github.com/masone85/final-react" target="_blank">
+            here!
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
